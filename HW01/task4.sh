27dfe8f63c260a1688db300a13cc282529d5b3c4
Seung-min Yu <67177134+smyu24@users.noreply.github.com>
2024-09-10 13:42:58 -0500

    task4.sh complete

---
+++ HW01/task4.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env zsh
+
+#SBATCH --job-name=FirstSlurm
+#SBATCH --partition=instruction
+#SBATCH --ntasks=1 --cpus-per-task=2
+#SBATCH -t 0-00:30:00
+#SBATCH -o FirstSlurm.out
+#SBATCH -e FirstSlurm.err
+
+cd $SLURM_SUBMIT_DIR
+
+echo $SLURM_SUBMIT_HOST
