678477beb9d707d8d8a0a693ae7c6f661fa3bf8a
Seung-min Yu <67177134+smyu24@users.noreply.github.com>
2024-09-20 15:04:16 -0500

    start of HW02

---
+++ HW02/scan.cpp
@@ -0,0 +1,3 @@
+
+
+1. a) Implement the scan function in a file called scan.cpp with signature defined as in scan.h. You should write an inclusive scan1 on your own and not use any library scan functions. 
---
+++ HW02/task1.cpp
@@ -0,0 +1,10 @@
+
+
+
+// b) Write a file task1.cpp with a main function which (in this order) 
+//     i) Creates an array of n random float numbers between -1.0 and 1.0. n should be read as the first command line argument as below. 
+//     ii) Scans the array using your scan function. 
+//     iii) Prints out the time taken by your scan function in milliseconds2. 
+//     iv) Prints the first element of the output scanned array. 
+//     v) Prints the last element of the output scanned array. 
+//     vi) Deallocates memory when necessary. 
