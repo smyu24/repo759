756d117a4b16b58b78017bf5e1c76250e626bc5d
Seung-min Yu <67177134+smyu24@users.noreply.github.com>
2024-09-20 15:19:14 -0500

    a

---
+++ HW02/task1.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env zsh
+
+#SBATCH --job-name=FirstSlurm
+#SBATCH --partition=instruction
+#SBATCH --ntasks=1 --cpus-per-task=2
+#SBATCH -t 0-00:30:00
+#SBATCH -o FirstSlurm.out -e FirstSlurm.err
+
+hostname
